d6358df035a49734ecf338daa7ed92f90925d052
Henrique Nascimento <henrique.nascimento613@gmail.com>
2019-04-29 12:02:10 +0100

    projeto

---
+++ projeto/HHSBooking/Models/EspecificacoesQuarto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HHSBooking.Models
+{
+    public class EspecificacoesQuarto
+    {
+        public int IDEspecificacao { get; set; }
+        public long IDTipoQuarto { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+    }
+}
---
+++ projeto/HHSBooking/Models/Regime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HHSBooking.Models
+{
+    public class Regime
+    {
+        public int IDRegime { get; set; }
+
+        public long IDHotel { get; set; }
+
+        [Required]
+        [Display(Name = "Tipo do Regime")]
+        public string TipoRegime { get; set; }
+    }
+}
