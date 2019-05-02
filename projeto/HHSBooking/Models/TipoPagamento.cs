23d4a05299ccf27eda34aa7ecdbdab3da9a9c772
Catarina Batista <catarinabatistayou@gmail.com>
2019-05-02 14:33:55 +0100

    asf

---
+++ projeto/HHSBooking/Models/TipoPagamento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HHSBooking
+{
+    public partial class TipoPagamento
+    {
+        public TipoPagamento()
+        {
+            Pagamento = new HashSet<Pagamento>();
+        }
+
+        public short IdtipoPagamento { get; set; }
+        public string Designacao { get; set; }
+
+        public virtual ICollection<Pagamento> Pagamento { get; set; }
+    }
+}
